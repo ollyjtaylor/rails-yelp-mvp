d31b99697061a5178cfbcbce10febd7bc0cd7d7d
Olly Taylor <ollyjtaylor@live.co.uk>
2019-12-10 12:28:57 +0000

    added untracked files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+rest1 = Restaurant.create(name: 'Hoxton 100', address: 'Shoreditch')
+rev1 = Review.create(content: 'ok i guess', restaurant: rest1)
+
+rest2 = Restaurant.create(name: 'Dishoom', address: 'Shoreditch')
+rev2 = Review.create(content: 'delicious', restaurant: rest2)
+
+rest3 = Restaurant.create(name: 'Albion', address: 'Shoreditch')
+rev3 = Review.create(content: 'mocha!', restaurant: rest3)
+
+rest4 = Restaurant.create(name: 'Pret', address: 'Shoreditch')
+rev4 = Review.create(content: 'average', restaurant: rest4)
